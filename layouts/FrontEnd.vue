5f0bcd9c776bc43b211e66fe283e701ac4bcdc13
RedBean <myself6331@gmail.com>
2021-11-10 22:19:37 +0800

    Feat: 新增基本元件

--- layouts/FrontEnd.vue
+++ layouts/FrontEnd.vue
@@ -1,7 +1,20 @@
 <template lang="pug">
   div
+    Navbar
     nuxt
+    Footerbar
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+import Footerbar from '@/components/Footerbar.vue'
+export default {
+  components: {
+    Navbar,
+    Footerbar
+  }
+}
+</script>
 
 <style lang="scss">
 @import '@/assets/scss/all.scss';
