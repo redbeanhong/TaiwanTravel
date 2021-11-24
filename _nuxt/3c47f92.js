fb8693cb177247877d52627efc35cc842d4fd351
RedBean <myself6331@gmail.com>
2021-11-24 22:05:35 +0800

    deploy

---
+++ _nuxt/3c47f92.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{213:function(n,e,t){"use strict";t.r(e);var r={layout:"FrontEnd"},l=t(7),component=Object(l.a)(r,(function(){var n=this,e=n.$createElement;n._self._c;return n._m(0)}),[function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("h2",[n._v("Food")])])}],!1,null,null,null);e.default=component.exports}}]);
